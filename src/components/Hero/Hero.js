26bc05c72f06a971dcac1068787170081a8ee048
Adonis3k <adonisdmadera@gmail.com>
2024-05-17 10:14:48 -0400

    adding images and JPGs

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Hero.css';
+import IMG_1041 from '../../assets/images/IMG_1041.heic';
+
 
 const Hero = () => {
   return (
     <div className='hero-container'>
-      
+      <div className='slide'>
+      <img src={IMG_1041} alt="Hero 1" className="hero-image" />
         
-      
+    </div>  
     </div>
   );
 };
