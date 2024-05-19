5bce978019dbeac17b81fcece756e25a418e0aa7
Adonis3k <adonisdmadera@gmail.com>
2024-05-19 10:00:43 -0400

    Working on Headers

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Hero.css';
-import IMG_1041 from '../../assets/images/IMG_1041.heic';
+// import IMG_1895 from '../../assets/images/IMG_1895.jpg';
 
 
 const Hero = () => {
   return (
     <div className='hero-container'>
       <div className='slide'>
-      <img src={IMG_1041} alt="Hero 1" className="hero-image" />
+      {/* <img src={IMG_1041} alt="Hero 1" className="hero-image" /> */}
         
     </div>  
     </div>
