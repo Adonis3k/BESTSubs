190773d59c212f06e06833182f9f79c2ae3f593d
Adonis3k <adonisdmadera@gmail.com>
2024-06-20 18:39:23 -0400

    working on banners

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import ImageSlider from '../ImageSlider/ImageSlider'; // Correct path
+// import ImageSlider from '../ImageSlider/ImageSlider'; // Correct path
 import './Hero.css';
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <ImageSlider />
-    </div>
+    // <div className="hero">
+    //   <ImageSlider />
+    // </div>
+   <div>
+   <div className="phoneNumber">
+   <span>📞  212-987-9756</span>
+   </div>
+   <div className='adress'>
+    <ul>
+    <li>1928 3rd AVENUE</li>
+    <li> CORNER OF 106TH ST.</li>
+    <li>NEW YORK, N.Y. 10029</li>
+    </ul>
+   </div>
+   </div>
+
   );
 }
 
