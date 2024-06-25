a19164459ad731dbf7b6885f397c250ffa1a24ec
Adonis3k <adonisdmadera@gmail.com>
2024-06-24 20:27:03 -0400

    working on Hero Section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header/Header.js';
 import Logo from './components/Logo/Logo.js';
-import Hero from './components/Hero/Hero.js'
+import Hero from './components/Hero/Hero.js';
 import Breakfast from './components/Breakfast/Breakfast.js';
 import ColdSubsWraps from './components/ColdSubsWraps/ColdSubsWraps.js';
 import HotSubsWraps from './components/HotSubsWraps/HotSubsWraps.js';
 import Salads from './components/Salads/Salads.js';
 import DrinksCombos from './components/DrinksCombos/DrinksCombos.js';
-import FooterHeader from './components/FooterHeader/FooterHeader.js'
+import FooterHeader from './components/FooterHeader/FooterHeader.js';
 import Footer from './components/Footer/Footer.js';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './App.css';
 
 const App = () => {
   return (
@@ -21,7 +23,7 @@ const App = () => {
       <ColdSubsWraps />
       <HotSubsWraps />
       <Salads />
-      <DrinksCombos/>
+      <DrinksCombos />
       <FooterHeader />
       <Footer />
     </div>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,114 @@
 import React from 'react';
-import BestSubsLogo from '../../assets/images/BestSubsLogo.png';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import beer from '../../assets/images/beer.png';
+import LGSub from '../../assets/images/LGSub.png';
+import catering from '../../assets/images/catering.png';
+import CA_Sub_CBR_Whole from '../../assets/images/CA_Sub_CBR_Whole.png';
+import CA_Sub_RoastBeef_Whole from '../../assets/images/CA_Sub_RoastBeef_Whole.png'; 
+import CA_Sub_BigItalian_Whole from '../../assets/images/CA_Sub_BigItalian_Whole.png';
+import Sub_3meat_wheat_whole from '../../assets/images/Sub_3meat_wheat_whole.png';
+import CA_Sub_Turkey_Whole from '../../assets/images/CA_Sub_Turkey_Whole.png';
+import CA_Sub_Gardener_Whole from '../../assets/images/CA_Sub_Gardener_Whole.png';
+
+
 import './Logo.css';
 
 const Logo = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false
+  };
+
   return (
     <div className='Logo'>
+      <Slider { ...settings}>
+    <div>
       <img
-        alt='BestSubs Logo'
-        src={BestSubsLogo}
-        style={{ width: '12rem', height: '12rem' }} 
+        alt='beer'
+        src={beer}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
       />
+    </div>
+    <div>
+      <img
+        alt='LGSub'
+        src={LGSub}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+      />
+    </div>
+    <div>
+      <img
+        alt='CA_Sub_CBR_Whole'
+        src={CA_Sub_CBR_Whole}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+      />
+    </div>
+    <div>
+      <img
+        alt='catering'
+        src={catering}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+      />
+    </div>
+    <div>
+      <img
+        alt='CA_Sub_BigItalian_Whole'
+        src={CA_Sub_BigItalian_Whole}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+        
+      />
+    </div>
+    <div>
+      <img
+        alt='CA_Sub_RoastBeef_Whole'
+        src={CA_Sub_RoastBeef_Whole}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+        
+      />
+    </div>
+   
+    <div>
+      <img
+        alt='Sub_3meat_wheat_whole'
+        src={Sub_3meat_wheat_whole}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+        
+      />
+    </div>
+    <div>
+      <img
+        alt='CA_Sub_Turkey_Whole'
+        src={CA_Sub_Turkey_Whole}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+        
+      />
+    </div>    <div>
+      <img
+        alt='CA_Sub_Gardener_Whole'
+        src={CA_Sub_Gardener_Whole}
+        className='carousel-image'
+        style={{ width: '20rem', height: 'auto' }} 
+        
+      />
+    </div>
+
+    </Slider>
     </div>
   );
 }
